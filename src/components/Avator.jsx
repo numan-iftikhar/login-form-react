fb3d9f90879413329595384bd1e93efe8bfb5681
Numan <numaniftikhar300@gmail.com>
2022-09-10 13:33:33 +0500

    initial commit

---
+++ src/components/Avator.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Avator = () => {
+  return (
+    <div>Avator</div>
+  )
+}
+
+export default Avator
---
+++ src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Avator from './Avator'
+
+const Form = () => {
+  return (
+    <div>
+        Form
+        <Avator/>
+    </div>
+  )
+}
+
+export default Form
